a5968c7a97b7a234fff85b31ccd07f4e2dc5d4d3
Max Woolf <max@minimaxir.com>
2022-08-07 18:48:42 -0700

    improve query params

--- gpt3_input_data.sql
+++ gpt3_input_data.sql
@@ -1,13 +1,25 @@
-SELECT
+WITH
+  base_table AS (
+  SELECT
     title,
     score
-FROM
+  FROM
     `bigquery-public-data.hacker_news.full`
-WHERE
+  WHERE
     type = "story"
-    AND descendants >= 20
     AND score >= 10
     AND url IS NOT NULL
-    AND timestamp BETWEEN "2019-08-01" AND "2022-08-01"
+    AND timestamp BETWEEN "2021-08-01" AND "2022-08-01" ),
+  stories_gte100 AS (
+  SELECT
+    *
+  FROM
+    base_table
+  WHERE
+    score >= 100 )
+SELECT
+  *
+FROM
+  stories_gte100
 ORDER BY
-    score DESC
+  score DESC
